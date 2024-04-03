81d61fb9718e83f3ca20b1e2de0f708a2d3099d8
Romain <romain.gojard@gmail.com>
2024-04-03 11:14:31 +0200

    Add is_draw method

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ class TicTacToe:
         self.playerToPlay = player
 
     def play(self, caseNumber, symbole):
-        if(self.game[caseNumber] == None):
+        if (self.game[caseNumber] == None):
             self.game[caseNumber] = symbole
         else:
             raise ValueError("Case already played !")
-    
+
     def __str__(self):
-        conv = lambda i : i or ' '
+        conv = lambda i: i or ' '
         res = [conv(i) for i in self.game]
         print(" | " + str(res[0]) + " | " + str(res[1]) + " | " + str(res[2]) + " | \n" +
               " | " + str(res[3]) + " | " + str(res[4]) + " | " + str(res[5]) + " | \n" +
               " | " + str(res[6]) + " | " + str(res[7]) + " | " + str(res[8]) + " | \n")
-        
+
     def has_winner(self):
         WIN_SCHEMES = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,4,8],
-            [2,4,6]
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [2, 4, 6]
         ]
 
         t = self.game
@@ -44,12 +44,13 @@ class TicTacToe:
         self.game = [None, None, None, None, None, None, None, None, None]
         self.playerToPlay = _player
 
+    def is_draw(self):
+        if ((None in self.game) or self.has_winner()):
+            return False
+        else:
+            return True
 
-player = Player("test-user","X")
+
+player = Player("test-user", "X")
 game = TicTacToe(player)
 game.__str__()
-
-
-
-
-
