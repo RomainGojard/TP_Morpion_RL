924bc268f43c54fba831ab632e5c990eadea0874
Lexio <alexis.barreyre@gmail.com>
2024-04-03 11:10:06 +0200

    add has_winner

--- main.py
+++ main.py
@@ -21,6 +21,25 @@ class TicTacToe:
         print(" | " + str(res[0]) + " | " + str(res[1]) + " | " + str(res[2]) + " | \n" +
               " | " + str(res[3]) + " | " + str(res[4]) + " | " + str(res[5]) + " | \n" +
               " | " + str(res[6]) + " | " + str(res[7]) + " | " + str(res[8]) + " | \n")
+        
+    def has_winner(self):
+        WIN_SCHEMES = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+
+        t = self.game
+        result = False
+        for win_scheme in WIN_SCHEMES:
+            if t[win_scheme[0]] == t[win_scheme[1]] == t[win_scheme[2]]:
+                result = True
+                break
+
+        return result
+
 
 player = Player("test-user","X")
 game = TicTacToe(player)
